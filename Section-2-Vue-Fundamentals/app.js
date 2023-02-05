43e59ab48fb66d5c600aaa741fe01f6cb15e22a4
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-05 18:08:43 +0500

    Outputting raw HTML

--- Section-2-Vue-Fundamentals/app.js
+++ Section-2-Vue-Fundamentals/app.js
@@ -4,6 +4,7 @@ const vm = Vue.createApp({
             firstName: "Ahmad",
             lastName: "Ali",
             url: "https://www.google.com/",
+            rawUrl: '<a href="https://www.facebook.com/" target="_blank">Facebook </a>'
         }
     },
     methods: {
