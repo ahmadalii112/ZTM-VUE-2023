b17fd28725ac89e63c2e4cd2bb3fda33d768e1b6
AhmadAli <ahmed.ali@codilitysolutions.com>
2023-02-06 17:20:08 +0500

    Listening to Events

--- Section-2-Vue-Fundamentals/app.js
+++ Section-2-Vue-Fundamentals/app.js
@@ -4,12 +4,19 @@ const vm = Vue.createApp({
             firstName: "Ahmad",
             lastName: "Ali",
             url: "https://www.google.com/",
-            rawUrl: '<a href="https://www.facebook.com/" target="_blank">Facebook </a>'
+            rawUrl: '<a href="https://www.facebook.com/" target="_blank">Facebook </a>',
+            age: 20
         }
     },
     methods: {
         fullName() {
             return `${this.firstName} ${this.lastName.toUpperCase()} `;
+        },
+        increment () {
+            this.age++;
+        },
+        updateLastName (event) {
+            this.lastName = event.target.value;
         }
     },
 }).mount('#app');
