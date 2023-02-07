25d3899cb0f35384e7db435a1547aaf755b7f3da
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-07 21:16:54 +0500

    Computed Properties

--- Section-2-Vue-Fundamentals/app.js
+++ Section-2-Vue-Fundamentals/app.js
@@ -10,18 +10,22 @@ const vm = Vue.createApp({
         }
     },
     methods: {
-        fullName() {
-            return `${this.firstName} ${this.middleName}  ${this.lastName.toUpperCase()} `;
-        },
         increment () {
             this.age++;
         },
         updateLastName (msg, event) {
-            console.log(msg);
+            // console.log(msg);
             this.lastName = event.target.value;
         },
         updateMiddleName (event) {
             this.middleName = event.target.value;
         }
+    },
+    // if you have a functions that calculates the property you can use computed properties
+    computed: {
+        fullName() {
+            console.log("Full Name Computed Called");
+            return `${this.firstName} ${this.middleName}  ${this.lastName.toUpperCase()} `;
+        },
     },
 }).mount('#app');
