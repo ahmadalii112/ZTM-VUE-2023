65fdfb0901417e3bf559beb565f994ed89a551cd
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-08 22:24:13 +0500

    Conditional Rendering

--- Section-2-Vue-Fundamentals/app.js
+++ Section-2-Vue-Fundamentals/app.js
@@ -1,29 +1,7 @@
 let vm = Vue.createApp({
     data() {
         return {
-            isPurple: false,
-            selectedColorProperty: '',
-            size: 150,
+            mode: 1,
         }
     },
-    computed: {
-        circle_classes() {
-            return {
-                // classname: condition
-                purple: this.isPurple,
-            }
-        },
-        circle_size() {
-            return {
-                width: this.size + 'px',
-                height: this.size + 'px',
-                lineHeight: this.size + 'px'
-            }
-        },
-        circle_rotation() {
-            return {
-                transform: 'rotate(30deg)'
-            }
-        }
-    },
-}).mount('#app')
+}).mount('#app');
