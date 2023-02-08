392846975212ff7c4bfedef6faebdfd4081ce21d
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-08 22:55:27 +0500

    List Rendering

--- Section-2-Vue-Fundamentals/app.js
+++ Section-2-Vue-Fundamentals/app.js
@@ -1,7 +1,12 @@
 let vm = Vue.createApp({
     data() {
         return {
-            mode: 1,
+            birds: ['Pigeons', 'Eagles', 'Doves', 'Parrots'],
+            people: [
+                { name: 'John', age: 20 },
+                { name: 'Rick', age: 18 },
+                { name: 'Amy', age: 33 }
+            ]
         }
-    },
+    }
 }).mount('#app');
