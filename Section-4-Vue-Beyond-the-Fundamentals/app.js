9b27b8ab0594f61021501a61c196441eae5a1002
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-09 23:36:42 +0500

    Using Lifecycle Hooks

--- Section-4-Vue-Beyond-the-Fundamentals/app.js
+++ Section-4-Vue-Beyond-the-Fundamentals/app.js
@@ -3,7 +3,35 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
-  }
+  },
+  beforeCreate() {
+    /* This hook is called before the Vue instance is initialized. */
+    console.log('beforeCreate() function called!', this.message);
+  },
+  created() {
+    /* This hook will be set to a function */
+    console.log('created() function called!', this.message);
+  },
+  beforeMount() {
+    /* This hook gets triggered once the template has been compiled but hasn't been added onto the page yet. */
+    console.log('beforeMount() function called!', this.$el);
+  },
+  mounted() {
+    /* This hook gets called after the template is mounted onto the page. */
+    console.log('mounted() function called!', this.$el);
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate() function called!');
+  },
+  updated() {
+    console.log('updated() function called!');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() function called!');
+  },
+  unmounted() {
+    console.log('unmounted() function called!');
+  },
 })
 vm.mount('#app')
 // setTimeout(() => {
