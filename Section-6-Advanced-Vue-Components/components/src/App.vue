efae3d6a813ae5426b03416c858a239fcbe33ad3
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-17 22:14:48 +0500

    Child Components

--- Section-6-Advanced-Vue-Components/components/src/App.vue
+++ Section-6-Advanced-Vue-Components/components/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <p>{{ msg }}</p>
+  <Greeting/>
 </template>
 
 <script>
+// add component Locally 
+import Greeting from '@/components/Greeting.vue'
   export default {
     name: 'App',
+    components: {
+      Greeting
+    }
   }
 </script>
--- Section-6-Advanced-Vue-Components/components/src/main.js
+++ Section-6-Advanced-Vue-Components/components/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Greeting from '@/components/Greeting.vue'
+// import Greeting from '@/components/Greeting.vue'
 
 let vm = createApp(App)
-vm.component("Greeting", Greeting)
+// vm.component("Greeting", Greeting)
 vm.mount('#app')
