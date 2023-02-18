e4cb2af354458cf0ea374b5ed801b3822391538a
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-18 16:29:34 +0500

    age doubled using computed property for read only

--- Section-6-Advanced-Vue-Components/components/src/components/User.vue
+++ Section-6-Advanced-Vue-Components/components/src/components/User.vue
@@ -1,19 +1,25 @@
 <template>
   <button type="button" @click="onClickAge">Button</button>
   <p>This user is {{ age }} years old</p>
+  <p> {{ ageDoubled }}</p>
 </template>
 
 <script>
 export default {
   name: "User",
   props: ["age"],
-  emits: ['age-change'],
+  emits: ['age-change'], // this process is necessary when we need to make any changes in props
   methods: {
     onClickAge() {
       // next step to emit an event
       this.$emit("age-change", 3); // any parent components can listen for the event, In my case my parent component is App
       // we can also pass value 
     },
+  },
+  computed: {
+    ageDoubled(){
+      return this.age * 2
+    }
   },
 };
 </script>
