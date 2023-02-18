bdb56ec0f9db661f5d654e9027dc910d42e65737
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-18 15:14:30 +0500

    Props

--- Section-6-Advanced-Vue-Components/components/src/App.vue
+++ Section-6-Advanced-Vue-Components/components/src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <p>Hey</p>
+  <h3>Hey</h3>
   <Greeting/>
+  <user :age="age"></user>
 </template>
 
 <script>
 // add component Locally 
 import Greeting from '@/components/Greeting.vue'
+import User from '@/components/User.vue'
   export default {
     name: 'App',
     components: {
-      Greeting
+      Greeting,
+      User
+    },
+    data() {
+      return {
+        age:20
+      }
     }
   }
 </script>
---
+++ Section-6-Advanced-Vue-Components/components/src/components/User.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>This user is {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: ["age"],
+};
+</script>
+
+<style></style>
