6e7531a77447f554aabdfa814de9fe687d3ad801
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-24 22:21:20 +0500

    Named Slots

--- Section-6-Advanced-Vue-Components/slots/src/App.vue
+++ Section-6-Advanced-Vue-Components/slots/src/App.vue
@@ -1,26 +1,44 @@
 <template>
   <app-form>
-    <div class="help">
-      <p>This is some help text</p>
-    </div>
-    <div class="fields">
+    <template v-slot:help>
+      <p>{{ help}}</p>
+    </template>
+    <template v-slot:fields>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-    </div>
-    <div class="buttons">
+    </template>
+    <template v-slot:buttons>
       <button type="submit">submit</button>
-    </div>
+    </template>
+    <p>Dummy Text</p>
   </app-form>
-  <app-form> </app-form>
+
+  <app-form>
+    <template v-slot:help>
+      <p>Contact text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">submit</button>
+    </template>
+  </app-form>
 </template>
 <script>
-import AppForm from './components/Form.vue';
+import AppForm from "./components/Form.vue";
 export default {
   components: { AppForm },
   name: "App",
   components: {
     AppForm,
-  }
+  },
+  data() {
+    return {
+      help: "This is Some Help Text."
+    }
+  },
 };
 </script>
