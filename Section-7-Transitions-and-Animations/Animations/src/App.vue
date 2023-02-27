87a927ec25b49bed66bb7f5a97f2231292147252
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-27 21:15:02 +0500

    Animating with JavaScript

--- Section-7-Transitions-and-Animations/Animations/src/App.vue
+++ Section-7-Transitions-and-Animations/Animations/src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-                                      <!-- appear => sometimes U want to play the animation when the page is apper -->
-  <transition name="zoom" type="animation" appear >
-    <h2 v-if="flag">Hellooooo</h2>
+  <transition
+    @before-enter="beforeEnter"
+    @enter="Enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <h2 v-if="flag">Hey!</h2>
   </transition>
 </template>
 
@@ -14,14 +20,41 @@ export default {
       flag: true,
     };
   },
+  methods: {
+    beforeEnter(el) {
+        console.log("before-enter event fired", el);
+    },
+    Enter(el, done) {
+      console.log("enter event fired", el);
+
+      done();
+    },
+    afterEnter(el) {
+      console.log("after-enter event fired", el);
+
+    },
+    beforeLeave(el) {
+      console.log("before-leave event fired", el);
+
+    },
+    leave(el, done) {
+      console.log("leave event fired", el);
+
+      done();
+    },
+    afterLeave(el) {
+      console.log("after-leave event fired", el);
+
+    },
+  },
 };
 </script>
 
 <style>
-h2{
+h2 {
   width: 400px;
-  padding:20px;
-  margin:20px;
+  padding: 20px;
+  margin: 20px;
 }
 
 .fade-leave-to {
@@ -47,21 +80,19 @@ h2{
 /* Create a zooming animation */
 @keyframes zoom-in {
   from {
-    transform: scale(0,0);
+    transform: scale(0, 0);
   }
   to {
-    transform: scale(1,1);
+    transform: scale(1, 1);
   }
 }
 
 @keyframes zoom-out {
   from {
-    transform: scale(1,1);
-
+    transform: scale(1, 1);
   }
   to {
-    transform: scale(0,0);
+    transform: scale(0, 0);
   }
 }
-
 </style>
