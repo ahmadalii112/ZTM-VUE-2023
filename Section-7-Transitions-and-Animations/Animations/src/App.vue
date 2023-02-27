fc06e7b8b14483fe61a7aea7aa9b1b2015b9ee5e
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-27 22:27:16 +0500

    Transition CSS Class Names

--- Section-7-Transitions-and-Animations/Animations/src/App.vue
+++ Section-7-Transitions-and-Animations/Animations/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
+
+  <!-- <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello world!</h2>
+    <h2 v-else key="secondary">Another hello!</h2>
+  </transition> -->
+
+  <!-- <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
+  </transition> -->
+
   <!-- <transition
     @before-enter="beforeEnter"
-    @enter="Enter"
+    @enter="enter"
     @after-enter="afterEnter"
     @before-leave="beforeLeave"
     @leave="leave"
@@ -10,16 +20,23 @@
     :css="true"
     name="fade"
   >
-    <h2 v-if="flag">Hey!</h2>
+    <h2 v-if="flag">Hey</h2>
   </transition> -->
+
   <button @click="addItem">Add</button>
+
   <ul>
-    <transition-group name="fade">
-      <li v-for="(number, index) in numbers" 
+    <transition-group name="fade"
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
+      <li
+        v-for="(number, index) in numbers"
         :key="number"
-        @click="removeItem(index)">
-      {{ number}}
-    </li>
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
     </transition-group>
   </ul>
 </template>
@@ -30,67 +47,82 @@ export default {
   data() {
     return {
       flag: true,
-      numbers: [1,2,3,4,5],
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
-    addItem(){
-      const num = Math.floor(Math.random() * 100 + 1); 
-      const index = Math.floor(Math.random() * this.numbers.length); 
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
       this.numbers.splice(index, 0, num);
     },
-    removeItem(index){
-      this.numbers.splice(index,1);
+    removeItem(index) {
+      this.numbers.splice(index, 1);
     },
     beforeEnter(el) {
-        console.log("before-enter event fired", el);
+      console.log("before-enter event fired", el);
     },
-    Enter(el) {
+    enter(el) {
       console.log("enter event fired", el);
+
       // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
       //   duration: 1000,
-      // })
+      // });
 
-      // animation.onFinish = () => {
+      // animation.onfinish = () => {
       //   done();
       // };
     },
     afterEnter(el) {
       console.log("after-enter event fired", el);
-
     },
     beforeLeave(el) {
       console.log("before-leave event fired", el);
-
     },
     leave(el) {
       console.log("leave event fired", el);
-      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)"}], {
-      //   duration: 1000,
-      // })
 
-      // animation.onFinish = () => {
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+      //   duration: 1000,
+      // });
+
+      // animation.onfinish = () => {
       //   done();
       // };
-
     },
     afterLeave(el) {
       console.log("after-leave event fired", el);
-
     },
   },
 };
 </script>
 
 <style>
-li{
-  font-size:22px;
-  cursor: pointer
+.animate__flipOutX {
+  position: absolute;
 }
+
+.animate__animated {
+  animation-duration: 1.5s;
+}
+
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
 h2 {
   width: 400px;
   padding: 20px;
   margin: 20px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
 }
 
 .fade-leave-to {
@@ -98,13 +130,22 @@ h2 {
   opacity: 0;
 }
 
+.fade-move {
+  transition: all 1s linear;
+}
+
+.fade-leave-active {
+  position: absolute;
+}
+
 .zoom-enter-active {
   animation: zoom-in 1s linear forwards;
-  transition: all 2s linear; /* type attribute above use seconds which we type  */
+  transition: all 2s linear;
 }
+
 .zoom-leave-active {
-  transition: all 1s linear;
-  animation: zoom-out 2s linear forwards; /* type attribute above use seconds which we type  */
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
 }
 .zoom-enter-from {
   opacity: 0;
@@ -113,7 +154,6 @@ h2 {
   opacity: 0;
 }
 
-/* Create a zooming animation */
 @keyframes zoom-in {
   from {
     transform: scale(0, 0);
