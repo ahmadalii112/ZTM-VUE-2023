3ce98157493b20373bd88b0923eaeddd8063ba95
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-04-05 20:41:53 +0500

    Applying Rules

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
@@ -83,7 +83,7 @@
                         </button>
                     </form>
                     <!-- Registration Form -->
-                    <vee-form v-show="tab === 'register'">
+                    <vee-form v-show="tab === 'register'" :validation-schema="schema">
                         <!-- Name -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Name</label>
@@ -92,6 +92,7 @@
                                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                                 placeholder="Enter Name"
                             />
+                            <ErrorMessage class="text-red-600" name="name" />
                         </div>
                         <!-- Email -->
                         <div class="mb-3">
@@ -169,6 +170,15 @@ export default {
     data(){
         return {
             tab: 'login',
+            schema: {
+                name: 'required',
+                email: '',
+                age: '',
+                password: '',
+                confirm_password: '',
+                country: '',
+                tos: '',
+            }
         }
     },
     computed: {
