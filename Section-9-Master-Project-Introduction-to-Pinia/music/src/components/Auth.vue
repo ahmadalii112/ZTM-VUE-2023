1d7a995bbcb2e174d0d563da4e64987445b614b8
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-04-09 18:17:34 +0500

    Validating Emails

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
@@ -97,11 +97,13 @@
                         <!-- Email -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Email</label>
-                            <input
+                            <vee-field name="email"
                                 type="email"
                                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                                 placeholder="Enter Email"
                             />
+                            <ErrorMessage class="text-red-600" name="email" />
+
                         </div>
                         <!-- Age -->
                         <div class="mb-3">
@@ -172,7 +174,7 @@ export default {
             tab: 'login',
             schema: {
                 name: 'required|min:3|max:100|alpha_spaces',
-                email: '',
+                email: 'required|min:3|max:100|email',
                 age: '',
                 password: '',
                 confirm_password: '',
--- Section-9-Master-Project-Introduction-to-Pinia/music/src/includes/validation.js
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/includes/validation.js
@@ -1,5 +1,5 @@
 import {Form as VeeForm, Field as VeeField, defineRule, ErrorMessage} from "vee-validate";
-import {required, min, max, alpha_spaces as alphaSpaces } from "@vee-validate/rules"
+import {required, min, max, alpha_spaces as alphaSpaces, email} from "@vee-validate/rules"
 
 export default {
   install(app) {
@@ -11,5 +11,6 @@ export default {
     defineRule('min', min)
     defineRule('max', max)
     defineRule('alpha_spaces', alphaSpaces)
+    defineRule('email', email)
   }
 }
