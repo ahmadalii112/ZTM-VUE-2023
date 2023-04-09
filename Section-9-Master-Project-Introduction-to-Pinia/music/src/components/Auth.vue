5d8cb6e54344aa1ea7bc646f60a507bbe57fdb4b
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-04-09 20:11:52 +0500

    Custom Error Messages

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
@@ -187,9 +187,9 @@ export default {
                 email: 'required|min:3|max:100|email',
                 age: 'required|min_value:18|max_value:100',
                 password: 'required|min:9|max:100|excluded:password',
-                confirm_password: 'confirmed:@password',
-                country: 'required|excluded:Antarctica',
-                tos: 'required',
+                confirm_password: 'passwords_mismatch:@password',
+                country: 'required|country_excluded:Antarctica',
+                tos: 'tos',
             },
           userData: {
               country: 'USA',
