4c0f77ce2ccbad1e335a2cf6645369e02f0e66f6
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-04-09 19:37:16 +0500

    Rendering Multiple Error Messages

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Auth.vue
@@ -118,12 +118,15 @@
                         <!-- Password -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Password</label>
-                            <vee-field name="password"
-                                type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Password"
-                            />
-                            <ErrorMessage class="text-red-600" name="password" />
+                            <vee-field name="password" :bails="false" v-slot="{field, errors}">
+                                <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                                       type="password"
+                                       placeholder="Password"
+                                       v-bind="field" />
+                                <div class="text-red-600" v-for="error in errors" :key="error">
+                                    {{ error }}
+                                </div>
+                            </vee-field>
                         </div>
                         <!-- Confirm Password -->
                         <div class="mb-3">
@@ -183,7 +186,7 @@ export default {
                 name: 'required|min:3|max:100|alpha_spaces',
                 email: 'required|min:3|max:100|email',
                 age: 'required|min_value:18|max_value:100',
-                password: 'required|min:3|max:100',
+                password: 'required|min:9|max:100|excluded:password',
                 confirm_password: 'confirmed:@password',
                 country: 'required|excluded:Antarctica',
                 tos: 'required',
