2aaf0548a7fd7f8449e6ef293bb6552d4dc241fb
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-04-09 18:07:31 +0500

    Additional Rules

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/includes/validation.js
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/includes/validation.js
@@ -1,5 +1,5 @@
 import {Form as VeeForm, Field as VeeField, defineRule, ErrorMessage} from "vee-validate";
-import {required} from "@vee-validate/rules"
+import {required, min, max, alpha_spaces as alphaSpaces } from "@vee-validate/rules"
 
 export default {
   install(app) {
@@ -8,5 +8,8 @@ export default {
     app.component("ErrorMessage", ErrorMessage)
 
     defineRule('required', required)
+    defineRule('min', min)
+    defineRule('max', max)
+    defineRule('alpha_spaces', alphaSpaces)
   }
 }
