f7d80b745ba733bedd8e5be9281ddb4a2f9eb9a9
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-05-01 22:13:56 +0500

    #4 - Accessing Store State

--- pinia-crash-course-tasks/src/App.vue
+++ pinia-crash-course-tasks/src/App.vue
@@ -5,13 +5,27 @@
             <img src="./assets/pinia-logo.svg" alt="pinia logo">
             <h1> Pinia Tasks</h1>
         </header>
+        <!--  task list -->
+        <div class="task-list">
+            <div v-for="task in taskStore.tasks">
+                <TaskDetails :task="task"/>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
-export default {}
+import {useTaskStore} from "@/stores/TaskStore";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+export default {
+    components: {TaskDetails},
+    // Composition API
+    setup() {
+        const taskStore = useTaskStore();
+        return {
+            taskStore
+        }
+    }
+}
 </script>
-
-<style>
-
-</style>
