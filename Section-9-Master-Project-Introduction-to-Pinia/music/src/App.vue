6f503d86d82c3196aa712b5acca04b7836d3f8f4
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-05-01 15:12:07 +0500

    Persisting the User Authentication

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/App.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/App.vue
@@ -280,12 +280,23 @@
 <script>
 import AppHeader from "@/components/Header.vue";
 import Auth from "@/components/Auth.vue";
+import {mapWritableState} from "pinia";
+import useUserStore from "@/stores/user";
+import {auth} from './includes/firebase'
 export default {
     name: "App",
     components: {Auth, AppHeader},
     component: {
         AppHeader,
         Auth,
+    },
+    computed: {
+        ...mapWritableState(useUserStore, ['userLoggedIn'])
+    },
+    created() {
+      if (auth.currentUser) {
+        this.userLoggedIn = true;
+      }
     }
 }
 </script>
