14af7c0fe71f348fbcfbb795169da5e40f9200cc
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-05-01 21:38:30 +0500

    #2 - Project Setup

---
+++ pinia-crash-course-tasks/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <!--  heading  -->
+        <header>
+            <img src="./assets/pinia-logo.svg" alt="pinia logo">
+            <h1> Pinia Tasks</h1>
+        </header>
+    </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
