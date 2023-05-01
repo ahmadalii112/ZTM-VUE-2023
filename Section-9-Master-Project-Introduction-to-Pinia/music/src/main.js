672f703c04fdc4d4ad91d99f1fe2de102d5e787e
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-05-01 15:02:48 +0500

    Initializing Firebase First

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/main.js
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/main.js
@@ -1,18 +1,22 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
-import'./includes/firebase'
+import {auth} from './includes/firebase'
 
 import './assets/base.css'
 import './assets/main.css'
 
-const app = createApp(App)
+let app;
+auth.onAuthStateChanged(() => {
+    if (!app) {
+        app = createApp(App);
+        app.use(createPinia());
+        app.use(router);
+        app.use(VeeValidatePlugin);
+        app.mount('#app');
+    }
 
-app.use(createPinia())
-app.use(router)
-app.use(VeeValidatePlugin)
-
-app.mount('#app')
+});
