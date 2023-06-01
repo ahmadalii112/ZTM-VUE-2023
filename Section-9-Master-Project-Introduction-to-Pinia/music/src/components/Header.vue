76f4223378d67958af1554cc38ebfef4ae7e7c8f
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-01 11:31:59 +0500

    Custom Links

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Header.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Header.vue
@@ -3,7 +3,7 @@
     <header id="header" class="bg-gray-700">
         <nav class="container mx-auto flex justify-start items-center py-5 px-4">
             <!-- App Name -->
-            <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/about"
+            <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
             >Music</router-link
             >
 
@@ -11,12 +11,15 @@
                 <!-- Primary Navigation -->
                 <ul class="flex flex-row mt-1">
                     <!-- Navigation Links -->
+                  <li>
+                    <router-link class="px-2 text-white" href="#" to="/about">About</router-link>
+                  </li>
                     <li v-if="!userStore.userLoggedIn">
                         <a @click.prevent="toggleAuthModal" class="px-2 text-white" href="#">Login / Register</a>
                     </li>
                     <template v-else>
                         <li>
-                            <a class="px-2 text-white" href="#">Manage</a>
+                            <router-link class="px-2 text-white" href="#" to="/manage">Manage</router-link>
                         </li>
                         <li>
                             <a class="px-2 text-white"
