09e6b218619d9c0be82b80a80b297018023dfb94
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-03 15:03:12 +0500

    Handling Drag and Drop Events

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Upload.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Upload.vue
@@ -8,8 +8,17 @@
         </div>
         <div class="p-6">
             <!-- Upload Dropbox -->
+                <!-- Default browser events  -->
             <div
                     class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+                    :class="{'bg-green-400 border-green-400 border-solid': is_dragover }"
+                    @drag.prevent.stop=""
+                    @dragstart.prevent.stop=""
+                    @dragend.prevent.stop="is_dragover = false"
+                    @dragover.prevent.stop="is_dragover = true"
+                    @dragenter.prevent.stop="is_dragover = true"
+                    @dragleave.prevent.stop="is_dragover = false"
+                    @drop.prevent.stop="upload"
             >
                 <h5>Drop your files here</h5>
             </div>
@@ -51,7 +60,17 @@
 
 <script>
 export default {
-    name: "Upload"
+    name: "Upload",
+    data() {
+        return {
+            is_dragover: false
+        }
+    },
+    methods: {
+        upload() {
+            this.is_dragover = false;
+        }
+    }
 }
 </script>
 
