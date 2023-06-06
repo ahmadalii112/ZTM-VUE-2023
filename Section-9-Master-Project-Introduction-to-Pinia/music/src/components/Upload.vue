2f4fa4205d0239c1728605e415d70899e97d4dcf
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-06 22:15:39 +0500

    Fallback Upload

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Upload.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/Upload.vue
@@ -22,6 +22,7 @@
             >
                 <h5>Drop your files here</h5>
             </div>
+                <input type="file" multiple @change="upload($event)">
             <hr class="my-6"/>
             <!-- Progess Bars -->
             <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -57,7 +58,9 @@ export default {
         upload($event) {
             this.is_dragover = false;
             // const { files } = $event.dataTransfer
-            const files = [...$event.dataTransfer.files]; // convert object to array so we loop through
+            const files = $event.dataTransfer
+                ? [...$event.dataTransfer.files]
+                : [...$event.target.files]  ; // convert object to array so we loop through
             files.forEach((file) => {
                 if (file.type !== 'audio/mpeg') {
                     return;
