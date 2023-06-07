a3588720be1e53c7bea80c31168016cbb3f02f23
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-06-07 22:45:33 +0500

    Prop Validation

--- Section-9-Master-Project-Introduction-to-Pinia/music/src/components/CompositionItem.vue
+++ Section-9-Master-Project-Introduction-to-Pinia/music/src/components/CompositionItem.vue
@@ -51,7 +51,13 @@
 <script>
 
 export default {
-  name: "CompositionItem"
+  name: "CompositionItem",
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    }
+  }
 }
 </script>
 
