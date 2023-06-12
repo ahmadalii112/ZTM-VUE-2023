2117574e3dec688216f2f0658a3b9527b853933b
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-12 18:56:53 +0500

    Reactive References

--- Section-21-Composition-API/CompositionApi/src/App.vue
+++ Section-21-Composition-API/CompositionApi/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }}</p>
+    <button @click.prevent="increment" type="button">Click Me</button>
+  </div>
 </template>
 
 <script>
+import {ref} from "vue"; // For reactivity
+
 export default {
   name: "App",
+  setup() {
+    let num = ref(0); // ref function returns an object
+    console.log(num);
+    function increment(){
+      num.value++
+    }
+
+    return {
+      num,
+      increment,
+    }
+  }
 };
 </script>
