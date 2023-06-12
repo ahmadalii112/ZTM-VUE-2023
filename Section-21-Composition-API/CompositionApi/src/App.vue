dcb3beaf75674f4b0f605f7ffb43374af73a9a3c
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-12 19:10:36 +0500

    The Reactive Function

--- Section-21-Composition-API/CompositionApi/src/App.vue
+++ Section-21-Composition-API/CompositionApi/src/App.vue
@@ -2,11 +2,14 @@
   <div>
     <p>{{ num }}</p>
     <button @click.prevent="increment" type="button">Click Me</button>
+<!--    <p>{{ user.name }}</p>-->
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import {ref} from "vue"; // For reactivity
+// import {ref, reactive} from "vue"; // For reactivity
+import {ref, reactive, toRefs } from "vue"; // the two refs functions converts an object's properties into separate reactive references
 
 export default {
   name: "App",
@@ -17,10 +20,38 @@ export default {
       num.value++
     }
 
+    const user = reactive({
+      name: "Ahmad",
+      age: 20
+    });
+
+    setTimeout(() => {
+      user.name = "Ali"
+    }, 3000);
+
     return {
       num,
       increment,
+      // user,
+      ...toRefs(user), // Spread Operator
     }
   }
 };
+
+
+/*
+TODO:  REF vs Reactive
+
+ref()
+- Primitive Values
+- Values must accessed via the value property
+
+
+reactive()
+- Objects
+- Values from properties can be accessed directly
+
+*/
+
+
 </script>
