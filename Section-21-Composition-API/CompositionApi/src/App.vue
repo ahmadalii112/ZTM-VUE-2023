f9cf84bbf4525734192aa5dc0001499a6807ab64
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-12 19:37:16 +0500

    Watchers and Computed Properties

--- Section-21-Composition-API/CompositionApi/src/App.vue
+++ Section-21-Composition-API/CompositionApi/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <p>{{ num }}</p>
+    <p>{{ double }}</p>
     <button @click.prevent="increment" type="button">Click Me</button>
-<!--    <p>{{ user.name }}</p>-->
+    <!--    <p>{{ user.name }}</p>-->
     <p>{{ name }}</p>
+
+    <p>
+      <input type="text" v-model="phrase"/>
+    </p>
+    <p>{{ reveredPhrase }}</p>
   </div>
 </template>
 
 <script>
 // import {ref, reactive} from "vue"; // For reactivity
-import {ref, reactive, toRefs } from "vue"; // the two refs functions converts an object's properties into separate reactive references
+// import {ref, reactive, toRefs } from "vue"; // the two refs functions converts an object's properties into separate reactive references
+// import {ref, reactive, toRefs, watchEffect} from "vue"; // watchEffect => watch dependencies of the function
+// import {ref, reactive, toRefs, watchEffect, watch } from "vue";
+import {ref, reactive, toRefs, watchEffect, watch, computed } from "vue";
 
 export default {
   name: "App",
@@ -20,6 +29,10 @@ export default {
       num.value++
     }
 
+    const double = computed(() => {
+      return num.value * 2;
+    })
+
     const user = reactive({
       name: "Ahmad",
       age: 20
@@ -28,12 +41,30 @@ export default {
     setTimeout(() => {
       user.name = "Ali"
     }, 3000);
+    const phrase = ref("");
+    const reveredPhrase = ref("");
+
+    // One Way of using Watcher
+    watchEffect(() => {
+      reveredPhrase.value = phrase.value.split("").reverse().join("");
+    });
+    /* Second Way of using Watcher
+        - it watches only phrase because we pass it
+        - in function we can pass two args newVal and oldVal
+        - if we watch multiple dependencies then we pass first args as an array [phrase] and function parameters also will be as an array [newValue, oldValue]
+    watch([phrase], ([newValue, oldValue]) => {
+      reveredPhrase.value = phrase.value.split("").reverse().join("");
+    });
+    */
 
     return {
       num,
       increment,
       // user,
       ...toRefs(user), // Spread Operator
+      phrase,
+      reveredPhrase,
+      double
     }
   }
 };
