190dab119ebb9f1611b0cd1a1848a8004ef9cefa
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-13 11:59:26 +0500

    Pinia Hooks

--- Section-21-Composition-API/RouterHooks/src/views/About.vue
+++ Section-21-Composition-API/RouterHooks/src/views/About.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="about">
     <h1>{{ $route.params.member }}</h1>
+    <p> {{ store.counter }}</p>
+    <p> {{ store.doubleCount }}</p>
+    <button type="button" @click.prevent="store.increment">Press Me</button>
   </div>
 </template>
 
 <script>
 import {useRoute, useRouter} from "vue-router";
+import {useCounterStore} from "../stores/counter";
 export default {
   setup() {
     const route = useRoute(); // $this.$route
     const  router = useRouter() // $this.$router
+    const store = useCounterStore();
 
     console.log(route.params.member)
 
     router.push({
       hash: '#test'
     });
+
+    return {store};
   },
 };
 </script>
