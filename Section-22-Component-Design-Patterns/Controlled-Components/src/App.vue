f461117e809925a257442e8a7dc0cde210b61d0a
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-15 12:04:18 +0500

    Controlled Components

--- Section-22-Component-Design-Patterns/Controlled-Components/src/App.vue
+++ Section-22-Component-Design-Patterns/Controlled-Components/src/App.vue
@@ -10,22 +10,17 @@
                 type="text"
                 class="form-control"
                 placeholder="Name"
-                v-model="newsletterName"
+                :value="newsletterName"
+                @input="newsletterName = $event.target.value"
               />
+<!--                v-model="newsletterName"-->
             </div>
             <div class="mb-2">
-              <input
-                type="email"
-                class="form-control"
-                placeholder="E-mail"
-                v-model="newsletterEmail"
-                :class="{
-                  'is-valid':
-                    newsletterEmail.length >= 4 && newsletterEmail.length > 0,
-                  'is-invalid':
-                    newsletterEmail.length < 4 && newsletterEmail.length > 0,
-                }"
-              />
+              <EmailInput v-model="newsletterEmail"/>
+<!--              <EmailInput
+                  :modelValue="newsletterEmail"
+                  @update:modelValue="newsletterEmail = $event"
+              />-->
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
@@ -36,8 +31,10 @@
 </template>
 
 <script>
+import EmailInput from "./components/EmailInput.vue";
 export default {
   name: "App",
+  components: {EmailInput},
   data() {
     return {
       newsletterName: "",
---
+++ Section-22-Component-Design-Patterns/Controlled-Components/src/components/EmailInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+      type="email"
+      class="form-control"
+      placeholder="E-mail"
+      :value="modelValue"
+      @input="update($event)"
+      :class="{
+                  'is-valid':
+                    modelValue.length >= 4 && modelValue.length > 0,
+                  'is-invalid':
+                    modelValue.length < 4 && modelValue.length > 0,
+                }"
+  />
+</template>
+
+
+<script>
+
+export default {
+  name: "EmailInput",
+  props: ['modelValue'],
+  methods: {
+    update($event) {
+      this.$emit('update:modelValue', $event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
