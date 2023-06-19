27ceddfb6ee76785ebe164cc15c71f13b601d84e
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-06-19 16:00:32 +0500

    The Teleport Component

---
+++ Section-22-Component-Design-Patterns/Moving-Beyond-Vue’s-Event-System/src/components/Action.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mt-2 ml-2" style="opacity: 0.5">
+    <button type="button" class="btn btn-primary" @click="open = true">
+      Open
+    </button>
+    <teleport to="body">
+      <app-modal :show="open" @hide="open = false" />
+    </teleport>
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+export default {
+  name: "Action",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
