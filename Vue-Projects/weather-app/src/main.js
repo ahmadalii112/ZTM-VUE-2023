a1a5370b98703342ffd77c6d5d1d2a1d55e49c32
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-08-20 19:53:24 +0500

    #2 - Tailwind Setup

--- Vue-Projects/weather-app/src/main.js
+++ Vue-Projects/weather-app/src/main.js
@@ -1,3 +1,4 @@
+import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
---
+++ Vue-Projects/weather-app/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "weather-primary": "#00668A",
+                "weather-secondary": "#004E71",
+            },
+        },
+        fontFamily: {
+            Roboto: ["Roboto", "sans-serif"],
+        },
+        container: {
+            padding: "2rem",
+            center: true
+        },
+        screens: {
+            sm: "640px",
+            md: "768px",
+        }
+    },
+    plugins: [],
+}
+
